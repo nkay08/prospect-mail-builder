87aeb5ed2c024a6400aed54c91cf8a12d4d509a1
NK <n.klipp@nkay.info>
2020-11-12 17:29:43 +0100

    Add retry to build, since it seems to fail for electron-builder error: cannot move download into final location

--- build.sh
+++ build.sh
@@ -6,6 +6,12 @@ git config pull.ff only
 git pull
 yarn
 #yarn start
-yarn run dist:linux
+n=0
+until [ "$n" -ge 3 ]
+do
+   yarn run dist:linux && break  # substitute your command here
+   n=$((n+1)) 
+   sleep 15
+done
 
-cp /prospect/prospect-mail/dist/* /prospect/dist/
+cp -r /prospect/prospect-mail/dist/* /prospect/dist/
