d5de4a2cccd6d4bf7de3057455e7d99c4c2172ec
NK <n.klipp@nkay.info>
2020-11-12 21:38:54 +0100

    Fix docker-compose volume typo

--- build.sh
+++ build.sh
@@ -12,11 +12,14 @@ until [ "$n" -ge "$max_tries" ]
 do
    yarn run dist:linux && break  # substitute your command here
    n=$((n+1)) 
+   echo "Failed. Trying again"
    sleep 2
 done
 
 if [ "$n" -ge "$max_tries" ]; then
+	echo "FAILED. Exceeded maximum tries"
 	exit 1
 fi
 
-cp -r /prospect/prospect-mail/dist/* /prospect/dist/
+rm -r /prospect/prospect-mail/dist/linux-unpacked || true
+cp  /prospect/prospect-mail/dist/* /prospect/dist/
